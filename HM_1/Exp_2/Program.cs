2ac339bd271986e1a4540f0de8880a8e78b6c328
AZinoveva <aytsaan@yandex.ru>
2022-08-06 13:04:11 +0300

    Example 2 added

---
+++ HM_1/Exp_2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//Console.Write("How many numbers do you want to compare? ");
+//int N = Convert.ToInt32(Console.ReadLine());
+
+int number1 = new Random().Next(-11, 10);
+int number2 = new Random().Next(-11, 10);
+int number3 = new Random().Next(-11, 10);
+
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+Console.WriteLine(number3);
+
+int max = number1;
+
+if(number2>max)
+{
+    max = number2;
+}
+if(number3>max)    
+{
+    max = number3;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
