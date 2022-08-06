130c3b04361dd49bfe127867c98b650c55db2d80
AZinoveva <aytsaan@yandex.ru>
2022-08-06 13:33:03 +0300

    Example 4 added

---
+++ HM_1/Exp_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i < N + 1)
+{
+    if ((i % 2) == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+    i++;
+}
