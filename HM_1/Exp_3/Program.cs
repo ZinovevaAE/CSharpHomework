d966c78e161c950a74d9ded6e092dcbf6b47ff9a
AZinoveva <aytsaan@yandex.ru>
2022-08-06 13:14:37 +0300

    Example 3 added

---
+++ HM_1/Exp_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int number = new Random().Next(-20, 30);
+
+Console.WriteLine(number);
+
+if((number % 2) == 0)
+{
+    Console.WriteLine("This is an even number");
+}
+else
+{
+    Console.WriteLine("This is an uneven number");
+}
