30a8942e4702a2386d0fc4d4c633e65c9da5fe05
AZinoveva <aytsaan@yandex.ru>
2022-08-11 12:07:27 +0300

    add ex3

---
+++ HM_2/Ex_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Write("Введите цифру от 1 до 7, где 1 обозначает понедельник, 2 - вторник, ..., 7 - воскресение: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Это выходной? ");
+if (number == 6) Console.Write("Да");
+else 
+    if (number == 7) Console.Write("Да");
+    else Console.Write("Нет");
