e369688b37a3e45509c27e76675bcded31dab93a
AZinoveva <aytsaan@yandex.ru>
2022-08-11 10:46:47 +0300

    Added ex_1

---
+++ HM_2/Ex_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+using System;
+
+class Program
+{
+    static int FunctionSecondNumber(int number)
+    {
+        int SecondDigit = (number / 10) % 10;
+        return (SecondDigit);
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Введите 3х значное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int x;
+        x = FunctionSecondNumber(number);
+        Console.Write("Вторая цифра: " + x);
+    }
+
+}
+
